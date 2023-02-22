3b0d7593808dc3f6bcda45dddddf9f225da3aa4b
Gorthdzx <krislaksana123@gmail.com>
2023-02-22 08:56:44 +0700

    temporary change*2

--- src/main/java/Modul_1/Buku.java
+++ src/main/java/Modul_1/Buku.java
@@ -6,38 +6,30 @@ public class Buku {
     private  String pengarang;
     private  final  String isbn;
     private String penerbit;
-    //private String kategori;
+    private  String kategori;
     // Data Structure and Algorithms;
 
 
-    Buku(String judul, String isbn) {
+    public Buku(String judul, String isbn) {
         this.judul = judul;
         this.isbn = isbn;
     }
 
 
-    public String pinjam(String penerbit,String pengarang1){
-        System.out.println("Nama buku : " + judul + "\nNomor ISBN :" + isbn  +"\nPenerbit : " + penerbit + "\nPengarang : " + pengarang1);
+    public String pinjam(String penerbit,String pengarang1,String kategori2){
+        System.out.println("Nama buku : " + judul + "\nNomor ISBN :" + isbn  +"\nPenerbit : " + penerbit + "\nPengarang : " + pengarang1 + "\nKategori : " +kategori2);
         this.penerbit=penerbit;
         this.pengarang=pengarang1;
-
+        this.kategori=kategori2;
         return "Sedang Dipinjam";
     }
 
 
-    public String kembali(String penerbit2){
-        System.out.println("Nama buku : " + judul + "\nNomor ISBN : " + isbn+"\nPenerbit : " + penerbit2);
+    public String kembali(String penerbit2 ,String pengarang2, String kategori2){
+        System.out.println("Nama buku : " + judul + "\nNomor ISBN : " + isbn+"\nPenerbit : " + penerbit2 + "\nPengarang : " + pengarang2 +"\nKategori : " + kategori2);
         this.penerbit =penerbit2;
+        this.pengarang=pengarang2;
+        this.kategori=kategori2;
         return  "Sudah Dikembalikan";
-    }
-
-
-    public static void main(String[]args) {
-        Buku book1 = new Buku("Introduction to Algorithms, 3rd Edition (The MIT Press),3rd Edition", "9780262033848");
-        Buku book2 = new Buku("Java Fundamental", "1304300");
-
-        System.out.println(book1.pinjam("The MIT Press","Thomas H.Cormen  & Charles E.Leiserson"));
-        System.out.println();
-        System.out.println(book2.kembali("O,Reilly"));
     }
     }
--- src/main/java/New_Book/mainBook.java
+++ src/main/java/New_Book/mainBook.java
@@ -1,4 +1,16 @@
 package New_Book;
 
+import Modul_1.Buku;
+
+
+
 public class mainBook {
+    public static void main(String[]args){
+        Buku book1 = new Buku("Introduction to Algorithms, 3rd Edition (The MIT Press),3rd Edition", "9780262033848");
+        Buku book2 = new Buku("Introductory Statistics 10th Tenth Edition By Neil A Weiss , 10 " , "978-0321989178");
+
+        System.out.println(book1.pinjam("The MIT Press","Thomas H.Cormen  & Charles E.Leiserson","Data Structure and Algorithms"));
+        System.out.println("========================================================================================");
+        System.out.println(book2.kembali("Pearson","Neil A. Weiss","Data Structure and Algorithms"));
+    }
 }
