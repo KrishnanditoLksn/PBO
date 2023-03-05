75671ad13f421b664cb5f7c2c4259247bbeac8f1
Gorthdzx <krislaksana123@gmail.com>
2023-03-05 09:22:26 +0700

    adding  comment to help people understand this code

--- src/main/java/Modul_2/MainSegitigaMRemake.java
+++ src/main/java/Modul_2/MainSegitigaMRemake.java
@@ -1,27 +1,27 @@
-package Modul_2;
+package Modul_2;//membuat metode main di package modul_2
 
-public class MainSegitigaMRemake {
-    public static void main(String[]args){
-        Segitiga_Remake seg1= new Segitiga_Remake();
-        Segitiga_Remake seg2= new Segitiga_Remake();
+public class MainSegitigaMRemake {//membuat class MainSegitigaRemake
+    public static void main(String[]args){//membuat metode main
+        Segitiga_Remake seg1= new Segitiga_Remake();//inisiasi objek bernama seg1 di konstruktor
+        Segitiga_Remake seg2= new Segitiga_Remake();//inisiasi objek bernama seg2 di konstruktor
 
-        seg1.setX1(1);
-        seg1.setX2(7);
-        seg1.setX3(7);
-        seg1.setY1(2);
-        seg1.setY2(2);
-        seg1.setY3(9);
+        seg1.setX1(1);//membuat atribut 1 untuk x1
+        seg1.setX2(7);//membuat atribut 2 untuk x2
+        seg1.setX3(7);//membuat atribut 7 untuk x3
+        seg1.setY1(2);//membuat atribut 2 untuk y1
+        seg1.setY2(2);//membuat atribut 2 untuk y2
+        seg1.setY3(9);//membuat atribut 9 untuk y3
 
-        System.out.println("Luas segitiga dengan titik (1,2) Page 4 of 5 (7,2) dan (7,9) adalah : " + seg1.hitungLuas());
-        System.out.println("============================================================================================");
+        System.out.println("Luas segitiga dengan titik (1,2) Page 4 of 5 (7,2) dan (7,9) adalah : " + seg1.hitungLuas());//mencetak dan memanggil metode hitungLuas untuk objek seg1
+        System.out.println("============================================================================================");//mencetak pembatas
 
-        seg2.setX1(0);
-        seg2.setX2(5);
-        seg2.setX3(8);
-        seg2.setY1(0);
-        seg2.setY2(4);
-        seg2.setY3(2);
-        System.out.println("Luas segitiga dengan titik (0,0) ,  (5,4) dan (8,2) adalah :" + seg2.hitungLuas());
+        seg2.setX1(0);//membuat atribut 0 untuk x1
+        seg2.setX2(5);//membuat atribut 5 untuk x2
+        seg2.setX3(8);//membuat atribut 8 untuk x3
+        seg2.setY1(0);//membuat atribut 0 untuk y1
+        seg2.setY2(4);//membuat atribut 4 untuk y2
+        seg2.setY3(2);//membuat atribut 2 untuk y3
+        System.out.println("Luas segitiga dengan titik (0,0) ,  (5,4) dan (8,2) adalah :" + seg2.hitungLuas());//mencetak dan memanggil metode hitungLuas untuk objek seg2
 
     }
 }
--- src/main/java/Modul_2/Segitiga_Remake.java
+++ src/main/java/Modul_2/Segitiga_Remake.java
@@ -1,63 +1,63 @@
-package Modul_2;
+package Modul_2;//membuat program di dalam  package Modul_2
 
-public class Segitiga_Remake {
-    public double x1,y1,x2,y2,x3,y3;
+public class Segitiga_Remake {//mendeklarasikan class Segitiga_Remake
+    public double x1,y1,x2,y2,x3,y3;//memberi nama variabel dan untuk atribut
 
 
-    public void setX1(int x1){
+    public void setX1(int x1){//menambah metode set untuk nilai x1 segitiga bertipe void
         this.x1 = x1;
     }
 
-    public double getX1() {
+    public double getX1() {//menambah metode get untuk mengembalikan nilai x1 segitiga bertipe double
         return x1;
     }
 
-    public void setX2(int x2){
+    public void setX2(int x2){//menambah  metode set untuk nilai x2 bertipe void
         this.x2 = x2;
     }
 
-    public double getX2(){
+    public double getX2(){//menambah metode get untuk mengembalikan x2 segitiga  bertipe double
         return  x2;
     }
 
-    public void  setX3(int x3){
+    public void  setX3(int x3){//menambah metode set untuk nilai x3 bertipe void
         this.x3 = x3;
     }
 
-    public double getX3(){
+    public double getX3(){//menambah metode get untuk mengembalikan nilai x3 segitiga bertipe double
         return  x3;
     }
 
-    public void setY1(int y1){
+    public void setY1(int y1){//menambah metode set untuk nilai y1 bertipe void
         this.y1 = y1;
     }
 
-    public double getY1(){
+    public double getY1(){//menambah metode get untuk mengembalikan nilai y1 segitiga bertipe double
         return  y1;
     }
 
-    public void setY2(int y2){
+    public void setY2(int y2){//menambah metode set untuk nilai y2 bertipe void
         this.y2  = y2;
     }
 
-    public double getY2(){
+    public double getY2(){//menambah metode get untuk mengembalikan nilai y2 segitiga bertipe double
         return y2;
     }
 
-    public void setY3(int y3){
+    public void setY3(int y3){//menambah metode set untuk nilai y3 bertipe void
         this.y3 = y3;
     }
 
 
-    public double hitungSisi(double xa, double ya, double xb, double yb){
+    public double hitungSisi(double xa, double ya, double xb, double yb){//metode untuk menghitung sisi segitiga
         return Math.sqrt( Math.pow(xa-xb, 2) + Math.pow(ya-yb,2));
     }
 
-    public double hitungKeliling(){
+    public double hitungKeliling(){//mtode untuk nenghitung keliling segitiga
         return  hitungSisi(x1 , y1 , x2 , y2 ) + hitungSisi(x2 , y2 ,x3 , y3) + hitungSisi(x3 , y3 , x1 , y1);
     }
 
-    public double hitungLuas(){
+    public double hitungLuas(){//metode untuk menghitung luas segitiga
         double s = hitungKeliling();
         return  Math.sqrt((s *(s - hitungSisi(x1 , y2 , x2 , y2 )))  * (s - hitungSisi(x2 , y2 , x3 , y3 ))    *  (s  - hitungSisi(x3 , y3 , x1 , y1 )));
     }
